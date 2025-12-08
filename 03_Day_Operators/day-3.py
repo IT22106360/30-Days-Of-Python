916b9e482ba8c50b27196abb5df44bc5f07fda37
Rashmika Rupasinghe <127801884+IT22106360@users.noreply.github.com>
2025-12-08 13:22:34 +0530

    Refactor day-3.py with exercises and calculations
    Refactor day-3.py to include exercises and calculations for triangle and rectangle areas, as well as weekly earnings and lifespan in seconds.

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -1,124 +1,67 @@
-# Arithmetic Operations in Python
-# Integers
+# Day 3 Exercises
 
-print('Addition: ', 1 + 2)
-print('Subtraction: ', 2 - 1)
-print('Multiplication: ', 2 * 3)
-print ('Division: ', 4 / 2)                         # Division in python gives floating number
-print('Division: ', 6 / 2)
-print('Division: ', 7 / 2)
-print('Division without the remainder: ', 7 // 2)   # gives without the floating number or without the remaining
-print('Modulus: ', 3 % 2)                           # Gives the remainder
-print ('Division without the remainder: ', 7 // 3)
-print('Exponential: ', 3 ** 2)                     # it means 3 * 3
+my_age =  24
+my_height = float(180)
+com_number = 2+3j
 
-# Floating numbers
-print('Floating Number,PI', 3.14)
-print('Floating Number, gravity', 9.81)
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+b = int(input("Enter the length 2nd side  of traingle: "))
+c = int(input("Enter the length 3nd side  of traingle: "))
 
-# Complex numbers
-print('Complex number: ', 1 + 1j)
-print('Multiplying complex number: ',(1 + 1j) * (1-1j))
+perimeter_triangle = base+b+c
 
-# Declaring the variable at the top first
+print("The perimeter of the triangle is: ", perimeter_triangle)
 
-a = 3 # a is a variable name and 3 is an integer data type
-b = 2 # b is a variable name and 3 is an integer data type
+area_traingle = 0.5*base*height
 
-# Arithmetic operations and assigning the result to a variable
-total = a + b
-diff = a - b
-product = a * b
-division = a / b
-remainder = a % b
-floor_division = a // b
-exponential = a ** b
+print("The area of the traingle is ", int(area_traingle))
 
-# I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
-print(total) # if you don't label your print with some string, you never know from where is  the result is coming
-print('a + b = ', total)
-print('a - b = ', diff)
-print('a * b = ', product)
-print('a / b = ', division)
-print('a % b = ', remainder)
-print('a // b = ', floor_division)
-print('a ** b = ', exponential)
+length = int(input("Enter length: "))
+width = int(input("Enter length: "))
 
-# Declaring values and organizing them together
-num_one = 3
-num_two = 4
+perimeter_rectangle = 2*(length+width)
 
-# Arithmetic operations
-total = num_one + num_two
-diff = num_two - num_one
-product = num_one * num_two
-div = num_two / num_two
-remainder = num_two % num_one
+print("The perimeter of the triangle is: ", int(perimeter_rectangle))
 
-# Printing values with label
-print('total: ', total)
-print('difference: ', diff)
-print('product: ', product)
-print('division: ', div)
-print('remainder: ', remainder)
+len_python = len('python')
+len_dragon = len('dragon')
 
+print(len_dragon is not len_python)
 
-# Calculating area of a circle
-radius = 10                                 # radius of a circle
-area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
-print('Area of a circle:', area_of_circle)
+on_in = ('on' in 'python') and ('on' in 'dragon')
 
-# Calculating area of a rectangle
-length = 10
-width = 20
-area_of_rectangle = length * width
-print('Area of rectangle:', area_of_rectangle)
+print(on_in)
 
-# Calculating a weight of an object
-mass = 75
-gravity = 9.81
-weight = mass * gravity
-print(weight, 'N')
+sentence = "I hope this course is not full of jargon"
 
-print(3 > 2)     # True, because 3 is greater than 2
-print(3 >= 2)    # True, because 3 is greater than 2
-print(3 < 2)     # False,  because 3 is greater than 2
-print(2 < 3)     # True, because 2 is less than 3
-print(2 <= 3)    # True, because 2 is less than 3
-print(3 == 2)    # False, because 3 is not equal to 2
-print(3 != 2)    # True, because 3 is not equal to 2
-print(len('mango') == len('avocado'))  # False
-print(len('mango') != len('avocado'))  # True
-print(len('mango') < len('avocado'))   # True
-print(len('milk') != len('meat'))      # False
-print(len('milk') == len('meat'))      # True
-print(len('tomato') == len('potato'))  # True
-print(len('python') > len('dragon'))   # False
+jargon_in = 'jargon' in sentence
 
-# Boolean comparison
-print('True == True: ', True == True)
-print('True == False: ', True == False)
-print('False == False:', False == False)
-print('True and True: ', True and True)
-print('True or False:', True or False)
+print(jargon_in)
 
-# Another way comparison 
-print('1 is 1', 1 is 1)                   # True - because the data values are the same
-print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
-print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
-print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
-print('coding' in 'coding for all') # True - because coding for all has the word coding
-print('a in an:', 'a' in 'an')      # True
-print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+print(str(len('python')))
 
-print(3 > 2 and 4 > 3) # True - because both statements are true
-print(3 > 2 and 4 < 3) # False - because the second statement is false
-print(3 < 2 and 4 < 3) # False - because both statements are false
-print(3 > 2 or 4 > 3)  # True - because both statements are true
-print(3 > 2 or 4 < 3)  # True - because one of the statement is true
-print(3 < 2 or 4 < 3)  # False - because both statements are false
-print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
-print(not True)      # False - Negation, the not operator turns true to false
-print(not False)     # True
-print(not not True)  # True
-print(not not False) # False
+print(7//2 == int(2.7))
+
+type('10') == type(10)
+
+int(9.8) == 10
+
+work_hours = int(input("Enter hours: "))
+rate_per_hour = int(input("Enter rate per hour: "))
+
+weekly_earning = work_hours*rate_per_hour
+
+print("Your weekly earning is: ", weekly_earning)
+
+seconds_per_year = 365*24*3600
+seconds_100_years = 100*seconds_per_year
+
+num_years_lived = int(input("Enter number of years you have lived: "))
+
+num_seconds_tolive = seconds_100_years - num_years_lived*seconds_per_year
+
+print("You have lived for", num_seconds_tolive, "seconds")
+
+for i in range(1, 6):
+    print(i, 1, i, i*i, i*i*i)
